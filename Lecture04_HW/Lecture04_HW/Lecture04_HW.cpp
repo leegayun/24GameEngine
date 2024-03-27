da690951d93e7cc04812df5071db293b6128a8b6
이가윤 <lckw7422@naver.com>
2024-03-27 14:23:42 +0900

    프로그래밍패턴 4주차 과제

---
+++ Lecture04_HW/Lecture04_HW/Lecture04_HW.cpp
@@ -0,0 +1,71 @@
+﻿#pragma comment(lib,"Opengl32.lib")
+#include<GLFW/glfw3.h>
+#include <iostream>
+
+void errorCallback(int error, const char* description)
+{
+    std::cerr << "GLFW Error : " << description << std::endl;
+}
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+    else
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+}
+
+
+int main()
+{
+    /* glfw 라이브러리 초기화 */
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        //glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
