aca361561c396e1d79c3f794caaea6c47ff453d3
이가윤 <lckw7422@naver.com>
2024-04-20 19:25:02 +0900

    202327001  프로그래밍패턴 6주차 과제

---
+++ Lecture06-HW/Lecture06-HW/MSList.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+template <typename T>
+class MSNode
+{
+public:
+    MSNode(T data);
+    ~MSNode();
+
+    T data;
+    MSNode* next_node;
+};
+
+
+template <typename T>
+class MSList
+{
+public:
+    MSList();
+    ~MSList();
+
+    int get_size();
+    void add(T data);
+    // Operator[] ¿À¹ö·Îµù
+    T& operator[](int index);
+
+private:
+    MSNode<T>* _head;
+    int _size;
+
+};
